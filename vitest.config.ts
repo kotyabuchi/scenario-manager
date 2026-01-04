1aa7c08fa7ab9cd783b90eb7d5b50d5d3c2f137d
kotyabuchi <kotyabuchi@gmail.com>
2026-01-04 20:26:24 +0900

    Vitestコンフィグ修正

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,14 +1,4 @@
-import path from 'node:path';
-import { fileURLToPath } from 'node:url';
-
 import { defineConfig } from 'vitest/config';
-
-import { storybookTest } from '@storybook/experimental-addon-test/vitest-plugin';
-
-const dirname =
-  typeof __dirname !== 'undefined'
-    ? __dirname
-    : path.dirname(fileURLToPath(import.meta.url));
 
 // More info at: https://storybook.js.org/docs/writing-tests/test-addon
 export default defineConfig({
@@ -19,7 +9,6 @@ export default defineConfig({
         plugins: [
           // The plugin will run tests for the stories defined in your Storybook config
           // See options at: https://storybook.js.org/docs/writing-tests/test-addon#storybooktest
-          storybookTest({ configDir: path.join(dirname, '.storybook') }),
         ],
         test: {
           name: 'storybook',
